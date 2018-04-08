c1a043fad8248225372a409e44dfd29e223d68d4
Albert Wu <albertwujj@gmail.com>
2018-04-08 13:19:02 -0700

    lol

--- ppo2.py
+++ ppo2.py
@@ -163,6 +163,8 @@ def learn(*, policy, env, nsteps, total_timesteps, ent_coef, lr,
           vf_coef=0.5, max_grad_norm=0.5, gamma=0.99, lam=0.95,
           log_interval=10, nminibatches=4, noptepochs=4, cliprange=0.2,
           save_interval=10):
+    logger.configure(dir='saved_training')
+
     if isinstance(lr, float):
         lr = constfn(lr)
     else:
